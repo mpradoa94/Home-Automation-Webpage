516a98381e706ce4a7fc5d4a81b6bc47744b0459
mpradoa94 <mpradoa94@gmail.com>
2015-05-02 18:45:12 -0500

    Update newUser.php

--- application/controllers/newUser.php
+++ application/controllers/newUser.php
@@ -17,24 +17,38 @@ class newUser extends CI_Controller{
     $user_id = $this->session->userdata('id');
     $is_admin = $this->session->userdata('isAdmin');
     
-    $data['isAdmin'] = $is_admin;
+    $data['is_admin'] = $is_admin;
     $data['email'] = $this->session->userdata('email');
     $data['name'] = $this->session->userdata('name');
 
     $this->load->view('newUser',$data);
+	
   }
-
+	
   function create_new_user() {
     $userInfo = $this->input->post(null,true);
-
+	if(isset($_POST['isAdmin'])) {
+		$userInfo['isAdmin'] = true;
+	}
+	else {
+		$userInfo['isAdmin'] = false;
+	}
+	
     if( count($userInfo) ) {
       $this->load->model('user_m');
       $saved = $this->user_m->create_new_user($userInfo);
     }
 
-    /*if ( isset($saved) && $saved ) {
-       echo "success";
-    }*/
+    if ( isset($saved) && $saved ) {
+		echo "success";
+        //redirect('/newUser/show_newUser');
+		//echo (int) $userInfo['isAdmin'];
+		/*$saved = true;
+		$data['error'] = $saved;
+
+        $this->load->helper('form');
+        $this->load->view('newUser',$data);*/
+    }
   }
 
 
