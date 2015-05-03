76f38892d19730406ce121cda6b9b01a5dbbd6b7
mpradoa94 <mpradoa94@gmail.com>
2015-05-02 20:20:56 -0500

    Update newUser.php

--- application/controllers/newUser.php
+++ application/controllers/newUser.php
@@ -11,7 +11,20 @@ class newUser extends CI_Controller{
         redirect('/login/show_login');
     }
   }
+
+  function show_newUser() {
     
+    $user_id = $this->session->userdata('id');
+    $is_admin = $this->session->userdata('isAdmin');
+    
+    $data['is_admin'] = $is_admin;
+    $data['email'] = $this->session->userdata('email');
+    $data['name'] = $this->session->userdata('name');
+
+    $this->load->view('newUser',$data);
+	
+  }
+	
   function create_new_user() {
         $this->load->helper(array('form', 'url'));
 
